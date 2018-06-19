a68127879f3eac5bdced27ef261149c580643668
David Peppel <skeltonath@gmail.com>
2018-06-19 11:24:33 -0700

    Removing 'l' from end of git info

--- pure.zsh
+++ pure.zsh
@@ -134,7 +134,7 @@ prompt_pure_preprompt_render() {
 	# Add git branch and dirty status info.
 	typeset -gA prompt_pure_vcs_info
 	if [[ -n $prompt_pure_vcs_info[branch] ]]; then
-		preprompt_parts+=("%F{$git_color}"'git:${prompt_pure_vcs_info[branch]}${prompt_pure_git_dirty}%f'l)
+		preprompt_parts+=("%F{$git_color}"'git:${prompt_pure_vcs_info[branch]}${prompt_pure_git_dirty}%f')
 	fi
 	# Git pull/push arrows.
 	if [[ -n $prompt_pure_git_arrows ]]; then
